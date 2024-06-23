c78558505643d09e047e2da80c3c81d7e09f263a
OMAR AL-SALEH <omaralsaleh1129@gmail.com>
2024-06-23 21:31:36 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,7 +5,7 @@ import * as firebase from "firebase";
 const Config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: "https://expensify-e9551-default-rtdb.firebaseio.com",
+  databaseURL: "https://expensify-production-82320-default-rtdb.firebaseio.com",
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
