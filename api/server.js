bc7b781aa802af51ac66d33b300b12036fce55a3
OmarAl-Saleh <omaralsaleh1129@gmail.com>
2024-06-24 21:24:35 +0300

    production database authentication

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "Public");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.use(express.static(publicPath));
 
